6947c77059623f1dcdf6149aeba36a98d71dcc95
Toni Cancela <toni.cancela@properpath.net>
2025-10-29 22:27:08 +0100

    test: finalize jest setup

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,4 +1,8 @@
-import 'jest-preset-angular/setup-jest';
 import 'zone.js';
+import { TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+// Inicialización del entorno de test para TestBed
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
 // Opcional: mocks comunes pueden añadirse aquí en el futuro.
